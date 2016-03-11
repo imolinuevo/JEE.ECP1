d723e6bb13255971ebf22cac62e4841eb1d36fa8
Iñigo Molinuevo <i.m.parron@opendeusto.es>
2016-03-11 13:57:04 +0100

    Security config refactor.

--- src/main/java/config/SecurityInitializer.java
+++ src/main/java/config/SecurityInitializer.java
@@ -3,5 +3,5 @@ package config;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
  
 public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-   //do nothing
+   
 }
